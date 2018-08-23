6c6b5a34e8a0e298fae4fba43189e4d769260ffe
zhigang.li <zhigang.li@tendcloud.com>
2018-08-23 20:50:32 +0800

    add

---
+++ src/mock/index.js
@@ -0,0 +1,5 @@
+import Mock from 'mockjs'
+
+//
+
+export default Mock
---
+++ src/store/module/user.js
@@ -0,0 +1,15 @@
+const state = {
+  //
+}
+const mutations = {
+  //
+}
+const actions = {
+  //
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
