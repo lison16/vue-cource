c41c7d3dbf19a0786bcba5b3116b067cd19cf586
zhigang.li <zhigang.li@tendcloud.com>
2018-08-29 21:30:12 +0800

    count-to

--- src/router/router.js
+++ src/router/router.js
@@ -40,6 +40,11 @@ export default [
     props: true
   },
   {
+    path: '/count-to',
+    name: 'count_to',
+    component: () => import('@/views/count-to.vue')
+  },
+  {
     path: '/parent',
     name: 'parent',
     component: () => import('@/views/parent.vue'),
