4da9639ee6440e94619a6f64cae7c52e5cdf3994
zhigang.li <zhigang.li@tendcloud.com>
2018-09-13 14:33:38 +0800

    修改vue.config.js里的BASE_URL

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const resolve = dir => path.join(__dirname, dir)
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin/' : '/'
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/' : '/'
 
 module.exports = {
   lintOnSave: false,
